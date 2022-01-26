590899f2e0f381c505123f8a0270e363d53d41fc
notomo <notomo.motono@gmail.com>
2022-01-26 22:43:35 +0900

    Add trancate_left

---
+++ lua/stlparts/component/trancate_left.lua
@@ -0,0 +1,24 @@
+local M = {}
+M.__index = M
+
+function M.new(component)
+  local ellipsis = ".."
+  local tbl = {
+    _component = require("stlparts.core.component").get(component),
+    _ellipsis = ellipsis,
+    _ellipsis_length = vim.fn.strwidth(ellipsis),
+  }
+  return setmetatable(tbl, M)
+end
+
+function M.build(self, ctx)
+  local str = self._component:build(ctx)
+  local width = vim.fn.strwidth(str)
+  local max_width = vim.api.nvim_win_get_width(ctx.window_id)
+  if max_width < width then
+    return ".." .. vim.fn.strpart(str, width - max_width + self._ellipsis_length)
+  end
+  return str
+end
+
+return M
