684a8bb00bf9218b8925816755a02fbcaa58387d
notomo <notomo.motono@gmail.com>
2022-07-31 17:23:37 +0900

    Add trancate_left options

--- lua/stlparts/component/trancate_left.lua
+++ lua/stlparts/component/trancate_left.lua
@@ -3,13 +3,18 @@ M.__index = M
 
 --- Trancate left string by window width.
 --- @param component function|table: |stlparts.nvim-types-component|
+--- @param opts table|nil: default: {max_width = function(ctx) return ctx:window_width() end, ellipsis = ".."}
 --- @return table: |stlparts.nvim-types-component|
-function M.new(component)
-  local ellipsis = ".."
+function M.new(component, opts)
+  opts = opts or {}
+  local ellipsis = opts.ellipsis or ".."
   local tbl = {
     _component = require("stlparts.core.component").get(component),
     _ellipsis = ellipsis,
     _ellipsis_length = vim.fn.strwidth(ellipsis),
+    _max_width = opts.max_width or function(ctx)
+      return ctx:window_width()
+    end,
   }
   return setmetatable(tbl, M)
 end
@@ -17,9 +22,9 @@ end
 function M.build(self, ctx)
   local str = self._component:build(ctx)
   local width = vim.fn.strwidth(str)
-  local max_width = ctx:window_width()
+  local max_width = self._max_width(ctx)
   if max_width < width then
-    return ".." .. vim.fn.strpart(str, width - max_width + self._ellipsis_length)
+    return self._ellipsis .. vim.fn.strpart(str, width - max_width + self._ellipsis_length)
   end
   return str
 end
